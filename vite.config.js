3ce2b90bfc56c1606de1c3af2dac1c914330d9e5
yasir-nizame <130412330+yasir-nizame@users.noreply.github.com>
2024-07-16 17:27:13 +0500

    cmt

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // or '/subdirectory/' if your app is deployed in a subdirectory
   plugins: [react()],
 })
